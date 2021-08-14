610111a46a877dfaf7721f93de5686eb80a2341f
Ngoan Tran <ngoan98travinh@gmail.com>
2021-08-14 14:42:21 +0700

    create user item and user list

---
+++ src/components/UserItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function UserItem() {
+    return (
+        <div>
+            <img src="https://picsum.photos/200" alt="" />
+            <p>This is username</p>
+        </div>
+    )
+}
+
+export default UserItem
---
+++ src/components/UserList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import UserItem from './UserItem'
+
+function UserList() {
+    return (
+        <div>
+            <h2>User List</h2>
+            <UserItem/>
+        </div>
+    )
+}
+
+export default UserList
